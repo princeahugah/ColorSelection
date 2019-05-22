dfa337df9864151bba5399a87eb27809a069205f
Prince Ahugah <princenprinceg@gmail.com>
2019-05-22 03:31:21 +0000

    remove comments

--- src/components/List.vue
+++ src/components/List.vue
@@ -16,12 +16,6 @@
 import ListItem from './ListItem';
 
 export default {
-    // Implement a feature to allow item selection with the following requirements:
-    // 1. Clicking an item selects/unselects it.
-    // 2. Multiple items can be selected at a time.
-    // 3. Currently selected items should be visually highlighted.
-    // 4. Currently selected items' names should be shown at the top of the page.
-    // 5. Make sure to avoid unnecessary re-renders.
     name: 'List',
 
     components: {
